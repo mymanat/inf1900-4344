139a98c79688addd93b6261d1319dadfad8d1453
Samuel <samuelpoulin>
2019-03-18 23:35:11 -0400

    Cleaned conflicts

--- codeCommun/tp/tp9/main.cpp
+++ codeCommun/tp/tp9/main.cpp
@@ -1,32 +1,29 @@
 #include <avr/io.h>
 
-
-
 #include "Routine.h"
 #include "memoire_24.h"
 
 int main() {
-    Memoire24CXXX memoire24CXXX;
+  Memoire24CXXX memoire24CXXX;
 
-    uint8_t commande = 0;
-    uint8_t data = 0;
+  uint8_t commande = 0;
+  uint8_t data = 0;
 
-    uint16_t nbAdresses = 0;
+  uint16_t nbAdresses = 0;
 
-    memoire24CXXX.lecture(0, &data);
-    nbAdresses = data << 8;
-    memoire24CXXX.lecture(1, &data);
-    nbAdresses |= data;
+  memoire24CXXX.lecture(0, &data);
+  nbAdresses = data << 8;
+  memoire24CXXX.lecture(1, &data);
+  nbAdresses |= data;
 
-    Routine routine;
-    routine.getAdresse();
+  Routine routine;
+  routine.getAdresse();
 
-    for (routine.setAdresse(2); routine.getAdresse() < nbAdresses; routine.addToAdress(2)){
+  for (routine.setAdresse(2); routine.getAdresse() < nbAdresses;
+       routine.addToAdress(2)) {
+    memoire24CXXX.lecture(routine.getAdresse(), &commande);
+    memoire24CXXX.lecture(routine.getAdresse() + 1, &data);
 
-        memoire24CXXX.lecture(routine.getAdresse(), &commande);
-        memoire24CXXX.lecture(routine.getAdresse() + 1, &data);
-
-        routine.executerCommande(commande, data);
-    }
-
+    routine.executerCommande(commande, data);
+  }
 }
