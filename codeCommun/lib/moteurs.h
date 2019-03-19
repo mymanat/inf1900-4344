c952995939105f82456627b8f0e087ae70959190
Samuel <samuelpoulin>
2019-03-18 23:31:45 -0400

    Conflict resolution

--- codeCommun/lib/moteurs.h
+++ codeCommun/lib/moteurs.h
@@ -5,26 +5,15 @@
 #include <util/delay.h>
 #include "PWM.h"
 class moteurs {
-public:
-    moteurs();
+ public:
+  moteurs();
 
-<<<<<<< HEAD
-void ajustementPWM(int a, int b);
-void avancer(int vitesse);
-void reculer(int vitesse);
-void tournerDroite();
-void tournerGauche();
-void arreterMoteurs();
-=======
-    void ajustementPWM(int a, int b);
-    void avancer(int vitesse);
-    void reculer(int vitesse);
-    void tournerDroite();
-    void tournerGauche();
-    void arreterMoteurs();
-
-
+  void ajustementPWM(int a, int b);
+  void avancer(int vitesse);
+  void reculer(int vitesse);
+  void tournerDroite();
+  void tournerGauche();
+  void arreterMoteurs();
 };
->>>>>>> 0cac7c03faa3d34a5b7b3148574f7ae8b704e970
 
 #endif
