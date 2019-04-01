6b6dbef14354a29280f8ef58cc73bf4be8b7645d
Samuel <samuelpoulin>
2019-04-01 19:31:55 -0400

    Test capteur complete

--- codeCommun/lib/constantes.h
+++ codeCommun/lib/constantes.h
@@ -16,14 +16,24 @@
 #define DDR_DEL DDRA
 #define PORT_DEL PORTA
 
-
 #define PORT_SPEAKER PORTB
 #define DDR_SPEAKER DDRB
-
 
 #define MODE_SORTIE 0xff
 #define MODE_ENTREE 0x00
 
-enum Couleur {Rouge, Vert, Ambree, Eteint};
+#define POSITION_DEL_1 0
+#define POSITION_DEL_2 1
+#define POSITION_DEL_3 2
+#define POSITION_DEL_4 3
+#define POSITION_DEL_5 7
+
+enum Couleur
+{
+    Rouge,
+    Vert,
+    Ambree,
+    Eteint
+};
 
 #endif
---
+++ codeCommun/projet_final/CapteurLignes/main.cpp
@@ -0,0 +1,91 @@
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Utils.h"
+#include "memoire_24.h"
+#include "constantes.h"
+#include "wait.h"
+#include "LOG.h"
+#include "can.h"
+
+int main()
+{
+    int cste = 500;
+    DDRA = MODE_ENTREE;
+    DDRC = MODE_SORTIE;
+    DDRB = MODE_ENTREE;
+    DDRD = MODE_SORTIE;
+
+    can convertisseur;
+    int valeurNoir = convertisseur.lecture(3);
+
+    uint16_t valeurLue;
+    char str[16];
+
+    initialisationUART();
+
+    PORTC = 0xff;
+
+    while (1)
+    {
+        valeurLue = convertisseur.lecture(0);
+        itoa(valeurLue, str, 10);
+
+        log_uart(str);
+        log_uart("\n");
+
+        if (valeurLue > valeurNoir + cste)
+        {
+            setBit(&PORTC, 0, POSITION_DEL_1);
+        }
+        else
+        {
+            setBit(&PORTC, 1, POSITION_DEL_1);
+        }
+
+        valeurLue = convertisseur.lecture(1);
+
+        if (valeurLue > valeurNoir + cste)
+        {
+            setBit(&PORTC, 0, POSITION_DEL_2);
+        }
+        else
+        {
+            setBit(&PORTC, 1, POSITION_DEL_2);
+        }
+
+        valeurLue = convertisseur.lecture(2);
+
+        if (valeurLue > valeurNoir + cste)
+        {
+            setBit(&PORTC, 0, POSITION_DEL_3);
+        }
+        else
+        {
+            setBit(&PORTC, 1, POSITION_DEL_3);
+        }
+
+        valeurLue = convertisseur.lecture(3);
+
+        if (valeurLue > valeurNoir + cste)
+        {
+            setBit(&PORTC, 0, POSITION_DEL_4);
+        }
+        else
+        {
+            setBit(&PORTC, 1, POSITION_DEL_4);
+        }
+
+        valeurLue = convertisseur.lecture(4);
+
+        if (valeurLue > valeurNoir + cste)
+        {
+            setBit(&PORTC, 0, POSITION_DEL_5);
+        }
+        else
+        {
+            setBit(&PORTC, 1, POSITION_DEL_5);
+        }
+    }
+}
