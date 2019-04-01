a52d0c3fab455acf73a91e7cad7ea69009890f2d
simon <tran.simon2@gmail.com>
2019-04-01 13:48:46 -0400

    DEBUG

--- codeCommun/lib/LOG.h
+++ codeCommun/lib/LOG.h
@@ -10,6 +10,15 @@ char receptionUART();
 
 void initialisationUART(void);
 
+
 void log_uart(const char mots[]);
+
+
+#ifdef DEBUG
+# define DEBUG_PRINT(x)   log_uart(x)
+#else
+# define DEBUG_PRINT(x) do {} while (0) // code mort
+#endif
+
 
 #endif 
