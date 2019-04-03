0555f6701699904cdf048e695d802a9c7d44acdd
simon <tran.simon2@gmail.com>
2019-04-03 18:50:47 -0400

    Robot1

--- codeCommun/projet_final/robot1/Robot1.cpp
+++ codeCommun/projet_final/robot1/Robot1.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "Robot1.h"
+Robot1::Robot1(uint8_t section) : section(section) {}
+
+void Robot1::run() {
+
+}
+
+void Robot1::setSection(uint8_t section) {
+    Robot1::section = section;
+}
+
+
--- codeCommun/projet_final/robot1/Robot1.h
+++ codeCommun/projet_final/robot1/Robot1.h
@@ -5,10 +5,20 @@
 #ifndef MAIN_ROBOT1_H
 #define MAIN_ROBOT1_H
 
+#include <stdint-gcc.h>
 #include "../Robot.h"
 
 class Robot1 : Robot{
+public:
 
+    Robot1(uint8_t section);
+
+    void run() override;
+
+    void setSection(uint8_t section);
+
+private:
+    uint8_t section = 1;
 };
 
 
