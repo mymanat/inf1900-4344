8d564f8192fb7fd0e27e48aa34a48a53f0a5cc30
simon <tran.simon2@gmail.com>
2019-04-03 16:04:37 -0400

    modification capteur

---
+++ codeCommun/lib/TrackerSensor.h
@@ -0,0 +1,16 @@
+//
+// Created by simon on 4/3/19.
+//
+
+#ifndef MAIN_TRACKERSENSOR_H
+#define MAIN_TRACKERSENSOR_H
+
+
+class TrackerSensor {
+public:
+    TrackerSensor();
+
+};
+
+
+#endif //MAIN_TRACKERSENSOR_H
--- codeCommun/projet_final/CapteurLignes/main.cpp
+++ codeCommun/projet_final/CapteurLignes/main.cpp
@@ -9,23 +9,28 @@
 #include "LOG.h"
 #include "can.h"
 
+#include "DEL.h"
+
 int main()
 {
-    int cste = 500;
+    uint16_t cste = 500;
     DDRA = MODE_ENTREE;
-    DDRC = MODE_SORTIE;
-    DDRB = MODE_ENTREE;
-    DDRD = MODE_SORTIE;
+//    DDRB = MODE_ENTREE;
+//    DDRC = MODE_SORTIE;
+//    DDRD = MODE_SORTIE;
+
+    DEL del;
 
     can convertisseur;
-    int valeurNoir = convertisseur.lecture(3);
+    uint16_t  valeurNoir = convertisseur.lecture(3);
 
     uint16_t valeurLue;
     char str[16];
 
     initialisationUART();
 
-    PORTC = 0xff;
+//    PORTC = 0xff;
+
 
     while (1)
     {
@@ -37,55 +42,57 @@ int main()
 
         if (valeurLue > valeurNoir + cste)
         {
-            setBit(&PORTC, 0, POSITION_DEL_1);
+            del.eteindre(1);
+//            setBit(&PORTC, 0, POSITION_DEL_1);
         }
         else
         {
-            setBit(&PORTC, 1, POSITION_DEL_1);
+            del.allumer(1);
+//            setBit(&PORTC, 1, POSITION_DEL_1);
         }
 
         valeurLue = convertisseur.lecture(1);
 
         if (valeurLue > valeurNoir + cste)
         {
-            setBit(&PORTC, 0, POSITION_DEL_2);
+            del.eteindre(2);
         }
         else
         {
-            setBit(&PORTC, 1, POSITION_DEL_2);
+            del.allumer(2);
         }
 
         valeurLue = convertisseur.lecture(2);
 
         if (valeurLue > valeurNoir + cste)
         {
-            setBit(&PORTC, 0, POSITION_DEL_3);
+            del.eteindre(3);
         }
         else
         {
-            setBit(&PORTC, 1, POSITION_DEL_3);
+            del.allumer(3);
         }
 
         valeurLue = convertisseur.lecture(3);
 
         if (valeurLue > valeurNoir + cste)
         {
-            setBit(&PORTC, 0, POSITION_DEL_4);
+            del.eteindre(4);
         }
         else
         {
-            setBit(&PORTC, 1, POSITION_DEL_4);
+            del.allumer(4);
         }
 
         valeurLue = convertisseur.lecture(4);
 
         if (valeurLue > valeurNoir + cste)
         {
-            setBit(&PORTC, 0, POSITION_DEL_5);
+            del.eteindre(5);
         }
         else
         {
-            setBit(&PORTC, 1, POSITION_DEL_5);
+            del.allumer(5);
         }
     }
 }
